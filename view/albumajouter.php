b4409d207611cea94813beeebb6d9653f79e0ff0
IZUNA-coder <80841824+IZUNA-coder@users.noreply.github.com>
2024-02-18 01:15:52 +0100

    Ajout de la fonctionnalité d'ajout d'album avec téléchargement d'image

--- view/albumajouter.php
+++ view/albumajouter.php
@@ -1,35 +1,55 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Ajout Album</title>
 </head>
 
 <body>
-    
-<?php
+
+    <?php
 
 
 
-echo "<h1>Ajout d'un Album à : {$_SESSION['pseudo_artiste']}</h1>";
+    echo "<h1>Ajout d'un Album à : {$_SESSION['pseudo_artiste']}</h1>";
 
 
-$idartiste = $_GET['id'];
-$_SESSION['id_artiste_choisi'] = $idartiste;
+    $idartiste = $_GET['id'];
+    $_SESSION['id_artiste_choisi'] = $idartiste;
 
-echo $formRetour;
-echo '<br>';
-echo $formAjouter;
+    echo $formRetour;
+    echo '<br>';
+    echo $formAjouter;
 
 
 
-    if(isset($_SESSION["titre"])){
-    var_dump( $_SESSION["titre"],
-    $_SESSION["annee_album"],
-    $_SESSION["Image"])
-    ;}
+    if (isset($_SESSION["titre"])) {
+        var_dump(
+            $_SESSION["titre"],
+            $_SESSION["annee_album"],
+            $_SESSION["Image"]
+        );
+    }
 
-?>  
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nom_fichier = $_FILES['image']['name'];
+            $fichier_tmp = $_FILES['image']['tmp_name'];
+
+            $dossier_cible = "data/images/";
+            $fichier_cible = $dossier_cible . basename($nom_fichier);
+
+            if (move_uploaded_file($fichier_tmp, $fichier_cible)) {
+                echo "Le fichier " . basename($nom_fichier) . " a été copié.";
+            } else {
+                echo "Désolé, une erreur s'est produite lors de la copie de votre fichier.";
+            }
+        }
+
+
+    ?>
 
 </body>
+
 </html>
